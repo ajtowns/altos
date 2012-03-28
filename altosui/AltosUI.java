bdfcc9112c41c494de23594963980a730b625cc4
Keith Packard <keithp@keithp.com>
2012-03-28 00:49:29 -0700

    altosui: Disable launch controller button
    The launch controller should just be a separate program.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- altosui/AltosUI.java
+++ altosui/AltosUI.java
@@ -210,12 +210,12 @@ public class AltosUI extends AltosFrame {
 			});
 		b.setToolTipText("Check flight readiness of altimeter in idle mode");
 
-		b = addButton(3, 2, "Launch Controller");
-		b.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					LaunchController();
-				}
-			});
+//		b = addButton(3, 2, "Launch Controller");
+//		b.addActionListener(new ActionListener() {
+//				public void actionPerformed(ActionEvent e) {
+//					LaunchController();
+//				}
+//			});
 
 		b = addButton(4, 2, "Quit");
 		b.addActionListener(new ActionListener() {
