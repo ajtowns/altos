69a8907ecbb7ca0e8526aeea0dc7490a191a0f8b
Keith Packard <keithp@keithp.com>
2012-06-02 16:57:22 -0700

    altos: Get HMC5883 driver limping along
    Not pushing data into the ring yet, but the chip appears to work now.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- src/drivers/ao_hmc5883.c
+++ src/drivers/ao_hmc5883.c
@@ -21,6 +21,8 @@
 
 static uint8_t	ao_hmc5883_configured;
 
+static uint8_t	ao_hmc5883_addr;
+
 static void
 ao_hmc5883_reg_write(uint8_t addr, uint8_t data)
 {
@@ -32,17 +34,21 @@ ao_hmc5883_reg_write(uint8_t addr, uint8_t data)
 	ao_i2c_start(AO_HMC5883_I2C_INDEX, HMC5883_ADDR_WRITE);
 	ao_i2c_send(d, 2, AO_HMC5883_I2C_INDEX, TRUE);
 	ao_i2c_put(AO_HMC5883_I2C_INDEX);
+	ao_hmc5883_addr = addr + 1;
 }
 
 static void
 ao_hmc5883_read(uint8_t addr, uint8_t *data, uint8_t len)
 {
 	ao_i2c_get(AO_HMC5883_I2C_INDEX);
-	ao_i2c_start(AO_HMC5883_I2C_INDEX, HMC5883_ADDR_WRITE);
-	ao_i2c_send(&addr, 1, AO_HMC5883_I2C_INDEX, FALSE);
+	if (addr != ao_hmc5883_addr) {
+		ao_i2c_start(AO_HMC5883_I2C_INDEX, HMC5883_ADDR_WRITE);
+		ao_i2c_send(&addr, 1, AO_HMC5883_I2C_INDEX, FALSE);
+	}
 	ao_i2c_start(AO_HMC5883_I2C_INDEX, HMC5883_ADDR_READ);
 	ao_i2c_recv(data, len, AO_HMC5883_I2C_INDEX, TRUE);
 	ao_i2c_put(AO_HMC5883_I2C_INDEX);
+	ao_hmc5883_addr = 0xff;
 }
 
 static uint8_t ao_hmc5883_done;
@@ -51,7 +57,7 @@ static void
 ao_hmc5883_isr(void)
 {
 	ao_exti_disable(&AO_HMC5883_INT_PORT, AO_HMC5883_INT_PIN);
-	ao_hmc5883_done = 1;
+	++ao_hmc5883_done;
 	ao_wakeup(&ao_hmc5883_done);
 }
 
@@ -65,10 +71,14 @@ ao_hmc5883_sample(struct ao_hmc5883_sample *sample)
 	ao_hmc5883_done = 0;
 	ao_exti_enable(&AO_HMC5883_INT_PORT, AO_HMC5883_INT_PIN);
 	ao_hmc5883_reg_write(HMC5883_MODE, HMC5883_MODE_SINGLE);
+
 	cli();
 	while (!ao_hmc5883_done)
 		ao_sleep(&ao_hmc5883_done);
 	sei();
+
+	printf ("done %d\n", ao_hmc5883_done);
+
 	ao_hmc5883_read(HMC5883_X_MSB, (uint8_t *) sample, sizeof (struct ao_hmc5883_sample));
 #if __BYTE_ORDER == __LITTLE_ENDIAN
 	/* byte swap */
@@ -82,21 +92,27 @@ ao_hmc5883_sample(struct ao_hmc5883_sample *sample)
 static uint8_t
 ao_hmc5883_setup(void)
 {
+	uint8_t d;
 	uint8_t	present;
 	if (ao_hmc5883_configured)
 		return 1;
 
-	/* Enable the EXTI interrupt for the appropriate pin */
-	ao_enable_port(AO_HMC5883_INT_PORT);
-	ao_exti_setup(&AO_HMC5883_INT_PORT, AO_HMC5883_INT_PIN,
-		      AO_EXTI_MODE_FALLING, ao_hmc5883_isr);
-
 	ao_i2c_get(AO_HMC5883_I2C_INDEX);
 	present = ao_i2c_start(AO_HMC5883_I2C_INDEX, HMC5883_ADDR_READ);
-	ao_i2c_recv(NULL, 0, AO_HMC5883_I2C_INDEX, TRUE);
+	ao_i2c_recv(&d, 1, AO_HMC5883_I2C_INDEX, TRUE);
 	ao_i2c_put(AO_HMC5883_I2C_INDEX);
+
 	if (!present)
 		return 0;
+
+	ao_hmc5883_reg_write(HMC5883_CONFIG_A,
+			     (HMC5883_CONFIG_A_MA_8 << HMC5883_CONFIG_A_MA) |
+			     (HMC5883_CONFIG_A_DO_15 << HMC5883_CONFIG_A_DO) |
+			     (HMC5883_CONFIG_A_MS_NORMAL << HMC5883_CONFIG_A_MS));
+
+	ao_hmc5883_reg_write(HMC5883_CONFIG_B,
+			     (HMC5883_CONFIG_B_GN_1_3 << HMC5883_CONFIG_B_GN));
+
 	ao_hmc5883_configured = 1;
 	return 1;
 }
@@ -132,9 +148,10 @@ ao_hmc5883_init(void)
 {
 	ao_hmc5883_configured = 0;
 
+	ao_enable_port(AO_HMC5883_INT_PORT);
 	ao_exti_setup(&AO_HMC5883_INT_PORT,
 		      AO_HMC5883_INT_PIN,
-		      AO_EXTI_MODE_FALLING,
+		      AO_EXTI_MODE_FALLING | AO_EXTI_MODE_PULL_UP,
 		      ao_hmc5883_isr);
 
 	ao_cmd_register(&ao_hmc5883_cmds[0]);
