4e6d96816e6604ee8d9bb49345a1c1211699a655
Keith Packard <keithp@keithp.com>
2012-06-17 16:11:23 -0700

    altos: ao_storage_read already calls ao_storage_setup
    No need to call twice.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- src/core/ao_config.c
+++ src/core/ao_config.c
@@ -81,7 +81,6 @@ _ao_config_get(void)
 	if (ao_config_loaded)
 		return;
 #if HAS_EEPROM
-	ao_storage_setup();
 	ao_storage_read(ao_storage_config, &ao_config, sizeof (ao_config));
 #endif
 	if (ao_config.major != AO_CONFIG_MAJOR) {
