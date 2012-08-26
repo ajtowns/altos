bf7def1a7b93867dfe16fe6499ee028747634c41
Mike Beattie <mike@ethernal.org>
2012-08-26 15:28:58 +1200

    altosdroid: Remove Binder import from TelemetryService
    Signed-off-by: Mike Beattie <mike@ethernal.org>

--- altosdroid/src/org/altusmetrum/AltosDroid/TelemetryService.java
+++ altosdroid/src/org/altusmetrum/AltosDroid/TelemetryService.java
@@ -26,7 +26,6 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
-//import android.os.Binder;
 import android.os.IBinder;
 import android.os.Handler;
 import android.os.Message;
