0f3597389977f86a8c1bdff1b7f46107c43ef306
Mike Beattie <mike@ethernal.org>
2012-08-26 12:41:24 +1200

    altosdroid: Update copyrights
    Signed-off-by: Mike Beattie <mike@ethernal.org>

--- altosdroid/src/org/altusmetrum/AltosDroid/AltosBluetooth.java
+++ altosdroid/src/org/altusmetrum/AltosDroid/AltosBluetooth.java
@@ -1,5 +1,6 @@
 /*
  * Copyright © 2011 Keith Packard <keithp@keithp.com>
+ * Copyright © 2012 Mike Beattie <mike@ethernal.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
