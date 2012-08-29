98f65994ee547feb8cca63ff4ed0fefd3fb2d37d
Keith Packard <keithp@keithp.com>
2012-08-28 23:05:02 -0700

    altos: Oops. forgot ao_data.c
    Signed-off-by: Keith Packard <keithp@keithp.com>

---
+++ src/stm/ao_data.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright © 2012 Keith Packard <keithp@keithp.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+
+#include <ao.h>
+#include <ao_data.h>
+
+volatile __xdata struct ao_data	ao_data_ring[AO_DATA_RING];
+volatile __data uint8_t		ao_data_head;
+volatile __data uint8_t		ao_data_present;
+
+void
+ao_data_get(__xdata struct ao_data *packet)
+{
+#if HAS_FLIGHT
+	uint8_t	i = ao_data_ring_prev(ao_sample_data);
+#else
+	uint8_t	i = ao_data_ring_prev(ao_data_head);
+#endif
+	memcpy(packet, (void *) &ao_data_ring[i], sizeof (struct ao_data));
+}
