5ad62b07bef41921b46cb7251072dc24290ee4c9
Mike Beattie <mike@ethernal.org>
2012-10-24 20:52:09 +1300

    altosdroid: Add branch to BuildInfo
    Signed-off-by: Mike Beattie <mike@ethernal.org>

--- altosdroid/buildinfo.sh
+++ altosdroid/buildinfo.sh
@@ -12,26 +12,31 @@ outfile=src/org/altusmetrum/AltosDroid/BuildInfo.java
 
 . ../src/Version
 version=$VERSION
+branch=''
 commitnum=''
 commithash=''
 builddate=$(date "+%Y-%m-%d")
 buildtime=$(date "+%H:%M")
+buildtz=$(date "+%z")
 
 
 describe=$(git describe --match "$version" --long --always 2>/dev/null || echo '')
 if [ -n "$describe" ]; then
+   branch=$(git status -s -b | sed -ne '1s/^## \(.*\)\.\.\..*$/\1/p')
    commitdetails=$(echo $describe | sed -e "s/^$version-//")
    commitnum=$(echo $commitdetails | cut -d- -f1)
    commithash=$(echo $commitdetails | cut -d- -f2)
 fi
 
 
-echo "Version $describe, built on $builddate, $buildtime"
+echo "Version $describe, built on $builddate $buildtime $buildtz"
 
-sed -e "s/@DESCRIBE@/$describe/" \
-    -e "s/@VERSION@/$version/" \
+sed -e "s/@VERSION@/$version/" \
+    -e "s/@DESCRIBE@/$describe/" \
+    -e "s/@BRANCH@/$branch/" \
     -e "s/@COMMITNUM@/$commitnum/" \
     -e "s/@COMMITHASH@/$commithash/" \
     -e "s/@BUILDDATE@/$builddate/" \
     -e "s/@BUILDTIME@/$buildtime/" \
+    -e "s/@BUILDTZ@/$buildtz/" \
  $infile > $outfile
