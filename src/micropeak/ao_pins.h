24948ea1d41f2a7c96ac09e35d1250909e5726ae
Keith Packard <keithp@keithp.com>
2012-12-09 14:32:35 -0800

    altos: Store altitude in 32-bits for MicroPeak
    Needs all 32 bits to store .1 meter resolution
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- src/micropeak/ao_pins.h
+++ src/micropeak/ao_pins.h
@@ -57,6 +57,7 @@
 #define I2C_PIN_SDA		PINB0
 
 #define AO_CONST_ATTRIB		PROGMEM
+typedef int32_t alt_t;
 #define FETCH_ALT(o)		((alt_t) pgm_read_dword(&altitude_table[o]))
 
 #define AO_ALT_VALUE(x)		((x) * 10)
