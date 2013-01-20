e1133481f2208fd16be8196977696da2cce430f3
Keith Packard <keithp@keithp.com>
2013-01-20 15:37:40 -0800

    altosui: All of the Altos class is actually in AltosUILib now
    Remove all of the duplicate content, shrinking AltosLib to a simple alias
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- altosui/Altos.java
+++ altosui/Altos.java
@@ -21,73 +21,8 @@ import java.awt.*;
 import libaltosJNI.*;
 
 import org.altusmetrum.AltosLib.*;
+import org.altusmetrum.altosuilib.*;
 
-public class Altos extends AltosLib {
+public class Altos extends AltosUILib {
 
-	static final int tab_elt_pad = 5;
-
-	static Font label_font;
-	static Font value_font;
-	static Font status_font;
-	static Font table_label_font;
-	static Font table_value_font;
-
-	final static int font_size_small = 1;
-	final static int font_size_medium = 2;
-	final static int font_size_large = 3;
-
-	static void set_fonts(int size) {
-		int	brief_size;
-		int	table_size;
-		int	status_size;
-
-		switch (size) {
-		case font_size_small:
-			brief_size = 16;
-			status_size = 18;
-			table_size = 11;
-			break;
-		default:
-		case font_size_medium:
-			brief_size = 22;
-			status_size = 24;
-			table_size = 14;
-			break;
-		case font_size_large:
-			brief_size = 26;
-			status_size = 30;
-			table_size = 17;
-			break;
-		}
-		label_font = new Font("Dialog", Font.PLAIN, brief_size);
-		value_font = new Font("Monospaced", Font.PLAIN, brief_size);
-		status_font = new Font("SansSerif", Font.BOLD, status_size);
-		table_label_font = new Font("SansSerif", Font.PLAIN, table_size);
-		table_value_font = new Font("Monospaced", Font.PLAIN, table_size);
-	}
-
-	static final int text_width = 20;
-
-	static public boolean initialized = false;
-	static public boolean loaded_library = false;
-
-	public static boolean load_library() {
-		if (!initialized) {
-			try {
-				System.loadLibrary("altos");
-				libaltos.altos_init();
-				loaded_library = true;
-			} catch (UnsatisfiedLinkError e) {
-				try {
-					System.loadLibrary("altos64");
-					libaltos.altos_init();
-					loaded_library = true;
-				} catch (UnsatisfiedLinkError e2) {
-					loaded_library = false;
-				}
-			}
-			initialized = true;
-		}
-		return loaded_library;
-	}
 }
