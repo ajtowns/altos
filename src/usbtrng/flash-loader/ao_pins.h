b5ac64bc0a6fd0fc23f39283e469c9820bdc88fe
Keith Packard <keithp@keithp.com>
2014-03-09 21:08:25 -0700

    altos: Missing ao_pins files for usbtrng
    Signed-off-by: Keith Packard <keithp@keithp.com>

---
+++ src/usbtrng/flash-loader/ao_pins.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2013 Keith Packard <keithp@keithp.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+
+#ifndef _AO_PINS_H_
+#define _AO_PINS_H_
+
+#include <ao_flash_lpc_pins.h>
+
+#define AO_BOOT_PIN		1
+#define AO_BOOT_APPLICATION_GPIO	0
+#define AO_BOOT_APPLICATION_PIN		19
+#define AO_BOOT_APPLICATION_VALUE	1
+#define AO_BOOT_APPLICATION_MODE	AO_EXTI_MODE_PULL_UP
+
+#define HAS_USB_PULLUP		1
+#define AO_USB_PULLUP_PORT	0
+#define AO_USB_PULLUP_PIN	6
+
+#endif /* _AO_PINS_H_ */
