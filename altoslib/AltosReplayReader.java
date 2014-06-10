9d39bbd22e6cde1bbb39e7b5450f297d47365769
Keith Packard <keithp@keithp.com>
2014-06-10 10:19:43 -0700

    altoslib: Check for time going backwards when replaying from file
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- altoslib/AltosReplayReader.java
+++ altoslib/AltosReplayReader.java
@@ -39,7 +39,7 @@ public class AltosReplayReader extends AltosFlightReader {
 
 	public void update(AltosState state) throws InterruptedException {
 		/* Make it run in realtime after the rocket leaves the pad */
-		if (state.state > AltosLib.ao_flight_pad)
+		if (state.state > AltosLib.ao_flight_pad && state.time_change > 0)
 			Thread.sleep((int) (Math.min(state.time_change,10) * 1000));
 		state.set_received_time(System.currentTimeMillis());
 	}
